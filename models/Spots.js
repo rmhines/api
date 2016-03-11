4b9212a92ea081c21adeb7715c003ea0d9d48875
Bobby Hines <bobby.m.hines@gmail.com>
2016-03-11 12:17:19 -0800

    Add tags to model, remove some unneeded fields

--- models/Spots.js
+++ models/Spots.js
@@ -5,14 +5,9 @@ var mongoose = require('mongoose');
 *************************************************************************************/
 var SpotSchema = new mongoose.Schema({
   	name: String, // The name of the bar or restaurant
+    address: String, // The full address, if available
+    tags: [String], // i.e. "American", "Cocktails", "Sushi" etc.
     website: String, // The URL for the venue's website, if available
-    type: String, // Type of venue, e.g. restaurant, bar, cafe
-    theme: String, // The style or theme of the food or drink, e.g. "German" or "Tex-Mex"
-    location: {
-        city: String, // The city where the venue is located
-        neighborhood: String, // The specific neighborhood where the venue is located
-        address: String // The actual full address, if available
-    },
     metrics: {
         visited: {type: Number, default: 0}, // The tally of users who have tried this spot
         avgStars: Number, // The 1-5 average quality rating based on user feedback
